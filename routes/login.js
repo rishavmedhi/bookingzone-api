0c864bf72845f2def5e7f172e9273f016280d2e9
Rishav Medhi <rishav164@gmail.com>
2019-11-13 00:06:00 +0530

    POST function for login
    Post function that returns user details if the credentials are true

---
+++ routes/login.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var router = express.Router();
+
+var {mongoose} = require('../db/mongoose');
+var {User} = require('../model/user');
+
+
+/* fetching login data */
+router.post('/', function(req,res,next){
+    let username = req.body.username;
+    let password = req.body.password;
+
+    User.find({
+        username : username,
+        password: password
+    })
+    .then((user)=>{
+        res.send(user);
+    }, (e) => {
+        res.status(400).send(e);
+    })
+});
+
+module.exports = router;
