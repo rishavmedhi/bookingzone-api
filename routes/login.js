dafb2d1afb4d05f68b6f90c6d9a78de9a10a1f35
Rishav Medhi <rishav164@gmail.com>
2019-11-14 20:25:44 +0530

    Sending output in correct format

--- routes/login.js
+++ routes/login.js
@@ -10,12 +10,16 @@ router.post('/', function(req,res,next){
     let username = req.body.username;
     let password = req.body.password;
 
+    // Todo : filter the info coming from the db
     User.find({
         username : username,
         password: password
     })
     .then((user)=>{
-        res.send({'status':1,'user':user});
+        if(user.length!==0)
+            res.send({'status':1,'user':user[0],msg:'Login successful'});
+        else
+            res.send({'status':0,'msg':'Failed to login. Cannot find username or invalid password'});
     }, (e) => {
         res.status(400).send(e);
     })
